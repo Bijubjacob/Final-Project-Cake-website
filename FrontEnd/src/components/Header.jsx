3c4c098602ff46b27ae1233e30ff044e8dc95c55
Daniel Rusev <danielgrusev@lewisu.edu>
2025-06-16 18:51:20 -0500

    Updated Header.jsx

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
-import styles from "./Header.module.css"; // Assuming you have a CSS module for styling
+import styles from "./Header.module.css";
 
 const Header = () => {
   const { user, logout } = useContext(AuthContext);
