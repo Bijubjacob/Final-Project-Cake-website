a14d99b189661d3d8318482b218c36131407c71f
Daniel Rusev <danielgrusev@lewisu.edu>
2025-06-17 13:46:22 -0500

    Update main.jsx

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
-import { CartProvider } from './context/CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <CartProvider>
           <App />
-        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
