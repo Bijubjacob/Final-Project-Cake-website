130986018886dcd3bb25c8ce19ad0eaab704e754
Daniel Rusev <danielgrusev@lewisu.edu>
2025-06-20 14:10:43 -0500

    Update ConfirmationPage.jsx

--- FrontEnd/src/components/ConfirmationPage.jsx
+++ FrontEnd/src/components/ConfirmationPage.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 
 function ConfirmationPage() {
   const navigate = useNavigate();
-
   const order = JSON.parse(localStorage.getItem('latestOrder'));
 
   useEffect(() => {
