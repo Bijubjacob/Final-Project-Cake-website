c1e2a8335a5f90f8ac08190223e34ce414c470b9
Daniel Rusev <danielgrusev@lewisu.edu>
2025-06-20 14:04:25 -0500

    Update db.mjs

--- BackEnd/config/db.mjs
+++ BackEnd/config/db.mjs
@@ -6,15 +6,12 @@ dotenv.config();
 mongoose.set('strictQuery', true);
 
 const db = process.env.MONGODB_URI;
-
 const connectDB = async () => {
   try {
     await mongoose.connect(db);
-    
-    console.log('Mongo DB Connected...');
+    console.log('Connected to Mongo');
   } catch (err) {
     console.error(err.message);
-
     process.exit(1);
   }
 };
